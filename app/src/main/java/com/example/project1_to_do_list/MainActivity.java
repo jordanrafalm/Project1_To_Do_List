7a0050d14e0c8a6f3a56f0dd0e78036a11955ded
jordanrafalm <jordanrafalm@gmail.com>
2023-01-06 13:43:23 +0100

    Zadanie 3. Do wyświetlenia pytania o usunięcie elementu proszę użyć AlertDialogFragmentu zamiast AlertDialogu.

--- app/src/main/java/com/example/project1_to_do_list/MainActivity.java
+++ app/src/main/java/com/example/project1_to_do_list/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.project1_to_do_list;
 
-import androidx.appcompat.app.AlertDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,10 +13,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private RecyclerView recyclerView;
+    //private RecyclerAdapter recyclerAdapter;
+   // private ImageView imageView;
     EditText item;
     Button add;
     ListView listView;
-    ArrayList<String> itemList = new ArrayList<>();
+    private ArrayList<String> itemList = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
 
     @Override
@@ -25,41 +29,56 @@ public class MainActivity extends AppCompatActivity {
 
         item = findViewById(R.id.editText);
         add = findViewById(R.id.button);
-        listView = findViewById(R.id.list);
-
+        listView = findViewById(R.id.list2);
         itemList = FileHelper.readData(this);
+     //   recyclerView = findViewById(R.id.recyclerView);
+       // recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1
-                ,android.R.id.text1, itemList);
+                , android.R.id.text1, itemList);
         listView.setAdapter(arrayAdapter);
 
         add.setOnClickListener(v -> {
             String itemName = item.getText().toString();
             itemList.add(itemName);
             item.setText("");
-            FileHelper.writeData(itemList,getApplicationContext());
+            FileHelper.writeData(itemList, getApplicationContext());
             arrayAdapter.notifyDataSetChanged();
 
+            //  recyclerAdapter.notifyDataSetChanged();
         });
 
-
         listView.setOnItemClickListener((parent, view, position, id) -> {
-            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
-            alert.setTitle("Delete");
-            alert.setMessage("Do you want delete?");
-            alert.setCancelable(false);
-            alert.setNegativeButton("No", (dialog, which) -> dialog.cancel());
-            alert.setPositiveButton("Yes", (dialog, which) -> {
+
+            AlertDialog.Builder AlertDialogFragment = new AlertDialog.Builder(MainActivity.this);
+            AlertDialogFragment.setView(R.layout.fragment_blank);  //ustawienie fragmentu jako widok
+            AlertDialogFragment.setMessage("Do you want delete?");
+            AlertDialogFragment.setCancelable(false);
+            AlertDialogFragment.setNegativeButton("No", (dialog, which) -> dialog.cancel());
+            AlertDialogFragment.setPositiveButton("Yes", (dialog, which) -> {
                 itemList.remove(position);
                 arrayAdapter.notifyDataSetChanged();
                 FileHelper.writeData(itemList, getApplicationContext());
-
             });
 
-            AlertDialog alertDialog = alert.create();
-            alertDialog.show();
+            AlertDialog alertDialogFragment = AlertDialogFragment.create();
+            alertDialogFragment.show();
 
+            //recyclerAdapter = new RecyclerAdapter(itemList,MainActivity.this, imageView);
+            // recyclerView.setAdapter(recyclerAdapter);
         });
-
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
